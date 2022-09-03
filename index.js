577c836ff5fd682bac04637e5a8a6e5a2ce18d4a
Charles Sin <charlessin1993@gmail.com>
2022-09-03 12:28:57 +0800

    update root api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const expense = require("./api/expenseTracker");
 const product = require("./api/product");
 const recipe = require("./api/recipe");
 
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(express.json({ extended: false }));
 
@@ -12,13 +12,11 @@ app.use("/api/product", product);
 app.use("/api/recipe", recipe);
 app.use("/api/expensebak", expense);
 
-app.get("/hrllo",(req,res)=>{
-    let hello = process.env.HELLO
-    // 使用環境變數
-    let secret = process.env.SECRET;
-    let id = process.env.ID;
-    res.status(200).json({hello,secret,id});
-})
+app.get("/", (req, res) => {
+  let title = process.env.TITLE;
+  let description = process.env.DESCRIPTION;
+  res.status(200).json({ title, secret });
+});
 
 const PORT = process.env.PORT || 8282;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
