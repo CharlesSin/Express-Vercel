1217788800bd7c0c28210c095b34eed70702af67
Charles Sin <charlessin1993@gmail.com>
2022-09-03 16:18:42 +0800

    add environment data

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.use("/api/expense", EXPENSE_API);
 app.get("/", (req, res) => {
   let title = process.env.TITLE;
   let description = process.env.DESCRIPTION;
-  res.status(200).json({ title, description });
+  let environment = process.env;
+  res.status(200).json({ title, description, environment });
 });
 
 const PORT = process.env.PORT || 8282;
